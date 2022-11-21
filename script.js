fac4b68d04f56d5c6697f695e6bd3e9cf5076174
Bholanath <bholanathbarik9748@gmail.com>
2022-11-21 18:03:51 +0530

    add second part

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 var form = document.getElementById('addForm');
 var item = document.getElementById('items');
+var item1 = document.getElementById('items1');
+var filter = document.getElementById('filter');
 
 // add addEventListener
-form.addEventListener('submit',addItem);
+form.addEventListener('submit',addItem(item,item1));
 
 // delete addEventListener
 item.addEventListener('click',deleteItem);
 
+// filter addEventListener
+filter.addEventListener('keyup',filterItem);
+
 
 // Add event
-function addItem(e){
+function addItem(e,e1){
   e.preventDefault();
+  e1.preventDefault();
 
   // get input value
   var newitem = document.getElementById('item').value;
+  var newitem1 = document.getElementById('item1').value;
 
   // create new Li element
   var li = document.createElement('li');
@@ -26,6 +33,7 @@ function addItem(e){
   
   // add text with input li
   li.appendChild(document.createTextNode(newitem));
+  li.appendChild(document.createTextNode(newitem1));
   btn.appendChild(document.createTextNode('X'));
 
   li.appendChild(btn);
@@ -40,4 +48,19 @@ function deleteItem(e){
       item.removeChild(li);
     }
   }
+}
+
+// filter item
+function filterItem(e) {
+  var text = e.target.value.toLowerCase();
+  var items = item.getElementsByTagName('li');
+  
+  Array.from(items).forEach(function(e) {
+    var itemName = e.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    }else{
+      item.style.display = 'none';
+    }
+  });
 }
